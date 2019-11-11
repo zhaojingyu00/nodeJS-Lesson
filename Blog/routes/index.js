9cf9e6f7a0f7f574775d0a9ee03d64e69fb9fbbf
zhaojingyu00 <2819361152@qq.com>
2019-11-11 08:23:21 +0800

    a

--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -16,11 +16,13 @@ router.get('/list',function(req,res,next){
 
 
 router.post('/tijiao',function(req,res,next){
+  req.setEncoding('utf-8');
   if(req.body.username==data.users[0].username && req.body.pwd==data.users[0].password){
     res.render('list',{chapterList:data.chapterList});
   }
   else{
-    res.end("failed");
+    res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+    res.end("用户名、密码错误");
   }
 })
 
